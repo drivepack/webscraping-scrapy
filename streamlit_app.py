82b3b7b7e9e551118cd89d2a3bcf02361acf3d31
Temidayo Amure <65348074+drivepack@users.noreply.github.com>
2023-05-29 13:23:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,25 @@
+import streamlit as st
+from pymongo import MongoClient
+import pandas as pd
+
+# Set up MongoDB connection
+username = urllib.parse.quote_plus('temidayodeveloper')
+password = urllib.parse.quote_plus('temidayodeveloper')
+client = MongoClient(f'mongodb+srv://{username}:{password}@cluster0.mongodb.net/?retryWrites=true&w=majority')
+
+# Select the database and collection
+db = client['Cluster0']
+collection = db['google_shopping']
+
+# Fetch some data from the collection
+cursor = collection.find({})  # Fetch all documents
+
+# Convert the cursor into a list
+mongo_docs = list(cursor)
+
+# Convert the list of MongoDB documents into a DataFrame
+df = pd.DataFrame(mongo_docs)
+
+# Display the DataFrame in Streamlit
+st.dataframe(df)
 
